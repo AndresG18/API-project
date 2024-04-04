817d42bf9f0fe409841402df241424ed101c3fef
Andres Garcia <Andrese17g@gmail.com>
2024-04-04 10:40:56 -0700

    fixing bugs 3.0

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -11,45 +11,6 @@ const router = express.Router();
 // backend/routes/api/session.js
 // ...
 
-// Log in
-router.post(
-    '/',
-    async (req, res, next) => {
-        const { credential, password } = req.body;
-
-        const user = await User.unscoped().findOne({
-            where: {
-                [Op.or]: {
-                    username: credential,
-                    email: credential
-                }
-            }
-        });
-
-        if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-            const err = new Error('Login failed');
-            err.status = 401;
-            err.title = 'Login failed';
-            err.errors = { credential: 'The provided credentials were invalid.' };
-            return next(err);
-        }
-
-        const safeUser = {
-            id: user.id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email,
-            username: user.username,
-        };
-
-        await setTokenCookie(res, safeUser);
-
-        return res.json({
-            user: safeUser
-        });
-    }
-);
-
 // Log out
 router.delete(
     '/',
@@ -86,11 +47,13 @@ const validateLogin = [
     check('credential')
         .exists({ checkFalsy: true })
         .notEmpty()
-        .withMessage('Please provide a valid email or username.'),
+        .isLength({ min: 2 })
+        .withMessage('Email or username is required'),
     check('password')
         .exists({ checkFalsy: true })
         .notEmpty()
-        .withMessage('Please provide a password.'),
+        .isLength({ min: 6 })
+        .withMessage("Password is required"),
     handleValidationErrors
 ];
 
