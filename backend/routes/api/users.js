ac2a9e973b92beead62a614282e7d1aec81cf46f
Andres Garcia <Andrese17g@gmail.com>
2024-04-04 14:20:25 -0700

    fixing bugs 3.3

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,7 +5,6 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
-const { User } = require('../../db/models')
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
